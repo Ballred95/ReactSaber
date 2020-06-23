59483ae0fd4c9f56531c7ec172e61802c44f2feb
Ballred95 <blake.allred95@icloud.com>
2020-06-23 00:33:22 -0600

    added clasname saber

--- src/components/saber.js
+++ src/components/saber.js
@@ -3,7 +3,7 @@ import React from 'react'
 var randomColor = Math.floor(Math.random()*16777215).toString(16);
 
 export default function() {
-    return(<div style = {{backgroundColor: `#${randomColor}`, width: '100px', height: '300px'}}>
+    return(<div className = 'saber' style = {{backgroundColor: `#${randomColor}`, width: '100px', height: '300px'}}>
         
         
         </div>
