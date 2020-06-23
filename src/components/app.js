58ea6c6eb1a8b736234c3c2f3d43023a54c4f9b8
Ballred95 <blake.allred95@icloud.com>
2020-06-23 00:35:37 -0600

    added state

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import Saber from './saber';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props) 
+
+    this.state = {
+      isOn: false, 
+      
+    }
+  }
+
   render() {
     return (
       <div className='app'>
